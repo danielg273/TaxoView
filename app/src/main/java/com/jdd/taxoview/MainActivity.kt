f6f4961e8bee0a31de357f16461158c216ef0d7c
slash <daniel142006@icloud.com>
2024-05-16 19:26:34 -0600

    toy probando un pinche boton feo

--- app/src/main/java/com/jdd/taxoview/MainActivity.kt
+++ app/src/main/java/com/jdd/taxoview/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jdd.taxoview
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val Bacteria2 = findViewById<ImageButton>(R.id.Bacteria)
+        val Archaea2 = findViewById<ImageButton>(R.id.Archaea)
+        val Eukarya2 = findViewById<ImageButton>(R.id.Eukarya)
+        Eukarya2.setOnClickListener{
+            val intent  = Intent(this, Eukarya::class.java)
+            startActivity(intent)
+        }
+        Archaea2.setOnClickListener{
+            val intent  = Intent(this, Archaea::class.java)
+            startActivity(intent)
+        }
+        Bacteria2.setOnClickListener{
+            val intent  = Intent(this, Bacteria::class.java)
+            startActivity(intent)
         }
     }
 }
